858091f7c7d965245574593588bf5593f9ae55f7
Luis C Perri <prluisca@gmail.com>
2022-08-12 18:51:03 -0500

    uploading

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+const app = express()
+
+app.use(express.json())
+express.urlencoded({ extended: false })
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send(`<h1>✌ Hello world</h1>`)
+})
